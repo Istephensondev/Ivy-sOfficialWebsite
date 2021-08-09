b5baa64e7328d3d79ee0203ca1a2b37614ad2358
Ivy Stephenson <istephensondev@gmail.com>
2021-08-08 22:18:44 -0400

    new changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const multiparty = require("multiparty");
-const { getMaxListeners } = require("node:process");
+// const { getMaxListeners } = require("node:process");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
@@ -44,13 +44,13 @@ function sendMessage() {
     secure: true,
     service: 'gmail',
     auth: {
-          type: "OAUTH2",
-          user: 'philosivy@gmail.com', //set these in your .env file
-          clientId: 769553003033-k0iu17efpqg0s53tj7g9f90sa7feg84j.apps.googleusercontent.com,
-          clientSecret: IkKZtK0jWAoG95K4RPMJhOiM,
-          refreshToken: 1//04ABlATmHy9m4CgYIARAAGAQSNwF-L9IrCZHABlyFneJnZnNwkA2I0IwFyzimO1TtPs4eJXvC-WSCHGYG3kkmfiReCaRJfPlBkLk, 
-          accessToken: ya29.a0ARrdaM88mluuTYf9QVRS8GN-to50S3h5AMF9QWw99jz2MLnzDgt6oXNq0Hk_e2lYZSTGAiGI-cLdbvexhiqFtqdNgCfQ8RzTYFU4JNjq_sEPVV1Z4TDSw42w09ZADXHu8uSIZDXGPXaF69fZ1xpnLwapA4gh,
-          expires: 3599
+        type: "OAUTH2",
+        user: 'philosivy@gmail.com', //set these in your .env file
+        clientId: "769553003033-k0iu17efpqg0s53tj7g9f90sa7feg84j.apps.googleusercontent.com",
+        clientSecret: "IkKZtK0jWAoG95K4RPMJhOiM",
+        refreshToken: "1//04ABlATmHy9m4CgYIARAAGAQSNwF-L9IrCZHABlyFneJnZnNwkA2I0IwFyzimO1TtPs4eJXvC-WSCHGYG3kkmfiReCaRJfPlBkLk", 
+        accessToken: "ya29.a0ARrdaM88mluuTYf9QVRS8GN-to50S3h5AMF9QWw99jz2MLnzDgt6oXNq0Hk_e2lYZSTGAiGI-cLdbvexhiqFtqdNgCfQ8RzTYFU4JNjq_sEPVV1Z4TDSw42w09ZADXHu8uSIZDXGPXaF69fZ1xpnLwapA4gh",
+        expires: 3599
     }
   });
 
